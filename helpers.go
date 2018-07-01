c996b37f133e30a0179ac6b3878d73dc512b0f44
Pragmatic <metaceo01@gmail.com>
2018-07-01 02:15:43 +0200

    load abi from file

--- helpers.go
+++ helpers.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"html/template"
+	"io/ioutil"
+
 	"github.com/go-macaron/session"
 	"gopkg.in/macaron.v1"
 )
@@ -9,8 +12,9 @@ func newPageData(ctx *macaron.Context, sess session.Store) {
 	ctx.Data["URI"] = ctx.Req.RequestURI
 	ctx.Data["AnoteAddress"] = conf.AnoteAddress
 	ctx.Data["NetworkVersion"] = conf.EthNetwork
-	// abi, err := ioutil.ReadFile("PonziCoin.abi")
-	// if err == nil {
-	// 	ctx.Data["ContractAbi"] = template.JS(string(abi))
-	// }
+
+	abi, err := ioutil.ReadFile("abi/ANT.abi")
+	if err == nil {
+		ctx.Data["AnoteAbi"] = template.JS(string(abi))
+	}
 }
