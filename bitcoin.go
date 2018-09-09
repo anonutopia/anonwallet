eb38e751e70dfea4404d2213b090ba1367b4d401
Pragmatic <cryptopragmatic@protonmail.com>
2018-09-09 10:14:09 +0200

    fix

--- bitcoin.go
+++ bitcoin.go
@@ -10,7 +10,7 @@ type BitcoinGenerator struct {
 }
 
 func (bg *BitcoinGenerator) getAddress() (string, error) {
-	out, err := exec.Command("electrum createnewaddress").Output()
+	out, err := exec.Command("electrum", "createnewaddress").Output()
 	if err != nil {
 		return "", err
 	}
