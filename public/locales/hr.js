6a5137ab8726afb8a271d54d62ba05d1cb6e0e82
Pragmatic <cryptopragmatic@protonmail.com>
2018-10-15 17:35:20 +0200

    anonutopia - translate

--- public/locales/hr.js
+++ public/locales/hr.js
@@ -4,6 +4,8 @@ var allLocales = {
     bothFields :            "Oba polja su obavezna.",
     allFields :             "Sva polja su obavezna.",
     wrongSeed :             "Niste upisali dobre seed riječi.",
-    passNotMatch :          "Lozinke koje ste upisali nisu iste."
+    passNotMatch :          "Lozinke koje ste upisali nisu iste.",
+    nicknameExists :        "Ovaj nadimak je već u upotrebi.",
+    emailExists :           "Ova email adresa je već u upotrebi."
 };
 
