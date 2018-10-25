76a4bcbd249be4eb764cc2fd937eca1fb40c8330
Pragmatic <cryptopragmatic@protonmail.com>
2018-10-25 21:43:33 +0200

    fix

--- bitcoin.go
+++ bitcoin.go
@@ -16,7 +16,6 @@ type BitcoinGenerator struct {
 func (bg *BitcoinGenerator) getAddress() (string, error) {
 	cmdStr := fmt.Sprintf("/usr/local/bin/electrum createnewaddress")
 	cmd := exec.Command("bash", "-c", cmdStr)
-	// cmd.Env = append(os.Environ(), "HOME=/home/kriptokuna")
 	var stdout, stderr bytes.Buffer
 	cmd.Stdout = &stdout
 	cmd.Stderr = &stderr
@@ -32,7 +31,6 @@ func (bg *BitcoinGenerator) getAddress() (string, error) {
 func (bg *BitcoinGenerator) getBalance(address string) (float64, error) {
 	cmdStr := fmt.Sprintf("/usr/local/bin/electrum getaddressbalance %s", address)
 	cmd := exec.Command("bash", "-c", cmdStr)
-	// cmd.Env = append(os.Environ(), "HOME=/home/kriptokuna")
 	var stdout, stderr bytes.Buffer
 	cmd.Stdout = &stdout
 	cmd.Stderr = &stderr
