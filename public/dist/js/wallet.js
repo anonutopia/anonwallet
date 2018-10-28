da51988a36ca1bacf756f8e049adc25090fc014a
Pragmatic <cryptopragmatic@protonmail.com>
2018-10-28 04:14:02 +0100

    test

--- public/dist/js/wallet.js
+++ public/dist/js/wallet.js
@@ -872,6 +872,6 @@ function getCookie(name) {
 function setCookie(name, value, days) {
     var d = new Date;
     d.setTime(d.getTime() + 24*60*60*1000*days);
-    document.cookie = name + "=" + value + ";path=/;expires=" + d.toGMTString();
+    document.cookie = name + "=" + value + ";path=/;expires=" + d.toUTCString();
 }
 function deleteCookie(name) { setCookie(name, '', -1); }
