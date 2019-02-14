3f66a450d55d8f2002b24f21778d8aed000cd435
Keekash <keekash@protonmail.com>
2019-02-14 11:36:32 +0100

    testing

--- email.go
+++ email.go
@@ -14,7 +14,7 @@ import (
 var domains = [...]string{
 	"aol.com", "att.net", "comcast.net", "facebook.com", "gmail.com", "gmx.com", "googlemail.com",
 	"google.com", "hotmail.com", "hotmail.co.uk", "mac.com", "me.com", "mail.com", "msn.com",
-	"live.com", "sbcglobal.net", "verizon.net", "yahoo.com", "yahoo.co.uk",
+	"live.com", "sbcglobal.net", "verizon.net", "yahoo.com", "yahoo.co.uk", "mail-tester.com",
 
 	"email.com", "fastmail.fm", "games.com", "gmx.net", "hush.com", "hushmail.com", "icloud.com",
 	"iname.com", "inbox.com", "lavabit.com", "love.com", "outlook.com", "pobox.com", "protonmail.com",
